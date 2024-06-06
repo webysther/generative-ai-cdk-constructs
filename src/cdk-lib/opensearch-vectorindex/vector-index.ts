f0dfd2c1c3af6d6df0b6f6d6a5ec94be0a6aa0d2
Dinesh Sajwan <dinsajwa@amazon.com>
2024-06-06 17:01:05 -0400

    feat(osconstruct): remove custom resource from os construct

--- src/cdk-lib/opensearch-vectorindex/vector-index.ts
+++ src/cdk-lib/opensearch-vectorindex/vector-index.ts
@@ -13,11 +13,16 @@
 import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as assets from 'aws-cdk-lib/aws-s3-assets';
 import * as oss from 'aws-cdk-lib/aws-opensearchserverless';
 import { Construct } from 'constructs';
 import { buildCustomResourceProvider } from '../../common/helpers/custom-resource-provider-helper';
 import { generatePhysicalNameV2 } from '../../common/helpers/utils';
 import { VectorCollection } from '../opensearchserverless';
+import * as fs from 'fs';
+import * as child_process from 'child_process';
+import { NagSuppressions } from 'cdk-nag/lib/nag-suppressions';
 
 /**
  * Metadata field definitions.
@@ -139,6 +144,30 @@ export class VectorIndex extends cdk.Resource {
     this.indexName = props.indexName;
     this.vectorField = props.vectorField;
     this.vectorDimensions = props.vectorDimensions;
+
+    
+    // const customResourceLayer = new lambda.LayerVersion(this, 'CustomResourceLayer', {
+    //   code: lambda.Code.fromAsset(path.join(__dirname, '../../../lambda/opensearch-serverless-custom-resources/custom-resource-layer/')),
+    //   compatibleRuntimes: [lambda.Runtime.PYTHON_3_12],
+    // });
+
+    this.installDependencies('../../../lambda/opensearch-serverless-custom-resources/custom-resource-layer', [
+      'boto3>=1.33.6',
+      'opensearch-py>=2.4.2',
+      'tenacity>=8.2.3',
+    ]);
+
+    const customResourceLayer = this.createLambdaLayer('../../../lambda/opensearch-serverless-custom-resources/custom-resource-layer')
+
+    const OpenSearchIndexCRProvider = buildCustomResourceProvider({
+      providerName: 'OpenSearchIndexCRProvider',
+      codePath: path.join(
+        __dirname, '../../../lambda/opensearch-serverless-custom-resources'),
+      handler: 'custom_resources.on_event',
+      runtime: lambda.Runtime.PYTHON_3_12,
+      layers: [customResourceLayer]
+    });
+
     const crProvider = OpenSearchIndexCRProvider.getProvider(this);
     crProvider.role.addManagedPolicy(props.collection.aossPolicy);
 
@@ -199,19 +228,79 @@ export class VectorIndex extends cdk.Resource {
     vectorIndex.node.addDependency(manageIndexPolicy);
     vectorIndex.node.addDependency(props.collection);
     vectorIndex.node.addDependency(props.collection.dataAccessPolicy);
+
+    
+  }
+  
+
+  private createLambdaLayer(layerDir: string): lambda.LayerVersion {
+    
+    const accesslogBucket = new s3.Bucket(this, 'AccessLogsLayers', {
+      enforceSSL: true,
+      versioned: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+    NagSuppressions.addResourceSuppressions(accesslogBucket, [
+      {id: 'AwsSolutions-S1', reason: 'There is no need to enable access logging for the AccessLogs bucket.'},
+    ])
+    const layerBucket = new s3.Bucket(this, 'DocBucket', {
+      enforceSSL: true,
+      versioned: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      serverAccessLogsBucket: accesslogBucket,
+      serverAccessLogsPrefix: 'layerAssetsBucketLogs/',
+    });
+
+  
+    const s3Key = 'layer.zip';
+    const s3Asset = new assets.Asset(this, `Asset`, {
+      path: layerDir,
+      assetHash: `${layerDir}-hash`,
+    });
+  
+    s3Asset.node.addDependency(layerBucket);
+
+
+    // return new lambda.LayerVersion(this, `Layer`, {
+    //   code: lambda.Code.fromAsset(layerDir),
+    //   compatibleRuntimes: [lambda.Runtime.PYTHON_3_12],
+    // });
+
+    return new lambda.LayerVersion(this, `Layer`, {
+      code: lambda.Code.fromBucket(layerBucket, s3Key),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_12],
+    });
   }
 
+  private installDependencies(layerDir: string, requirements: string[]): void {
+    const sitePackagesDir = path.join(layerDir, 'python', 'lib', 'python3.12', 'site-packages');
+    fs.mkdirSync(sitePackagesDir, { recursive: true });
+    child_process.execSync(`pip install -t ${sitePackagesDir} ${requirements.join(' ')}`, {
+      stdio: 'inherit',
+    });
+  }
 }
+
+
 
 /**
  * Custom Resource provider for OpenSearch Index operations.
  *
  * @internal This is an internal core function and should not be called directly by Solutions Constructs clients.
  */
-export const OpenSearchIndexCRProvider = buildCustomResourceProvider({
-  providerName: 'OpenSearchIndexCRProvider',
-  codePath: path.join(
-    __dirname, '../../../lambda/opensearch-serverless-custom-resources'),
-  handler: 'custom_resources.on_event',
-  runtime: lambda.Runtime.PYTHON_3_12,
-});
+// export const OpenSearchIndexCRProvider = buildCustomResourceProvider({
+//   providerName: 'OpenSearchIndexCRProvider',
+//   codePath: path.join(
+//     __dirname, '../../../lambda/opensearch-serverless-custom-resources'),
+//   handler: 'custom_resources.on_event',
+//   runtime: lambda.Runtime.PYTHON_3_12,
+//   layers: [customResourceLayer]
+// });
